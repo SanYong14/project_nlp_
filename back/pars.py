971255cbf403e0402d5ca74b02a631634407abd2
SanYong <Sashok9901@gmail.com>
2021-12-11 14:44:26 +0300

    gitIgnore update

--- back/pars.py
+++ back/pars.py
@@ -1,2 +1,24 @@
 import requests
-from . import tb
+# from . import tb
+import xml.etree.ElementTree as ET
+tree = ET.parse("G:\pythonshit\hackathon21_nlp\dataset_utf8 (2).xml")
+root = tree.getroot()
+
+print(root.tag)
+for child in root:
+    print(child.tag)
+    if child.tag == "{urn:schemas-microsoft-com:office:spreadsheet}Worksheet":
+        for step_child in child:
+            print(step_child.tag)
+            if step_child.tag == "{urn:schemas-microsoft-com:office:spreadsheet}Table":
+                for step_step_child in step_child:
+                    print(step_step_child.tag)
+                    if step_step_child.tag == "{urn:schemas-microsoft-com:office:spreadsheet}Row":
+                        for cell in step_step_child:
+                            print(cell[0].text)
+                            # print(cell[4].atrib)
+
+
+
+# for event, elem in ET.iterparse("G:\pythonshit\hackathon21_nlp\dataset_utf8 (2).xml"):
+#     print(elem)
--- back/tb.py
+++ back/tb.py
@@ -1,2 +1,10 @@
 from textblob import TextBlob
+from textblob.classifiers import NaiveBayesClassifier
+
+from . import pars
 from textblob import Word
+
+doc = u'Дорожный знак "Пешеходный переход" демонтирован вместе с опорой после ДТП.'
+text_ru = TextBlob(doc)
+
+print(text_ru.translate(to='en'))
